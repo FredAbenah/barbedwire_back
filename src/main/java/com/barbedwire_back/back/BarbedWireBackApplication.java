595334ee1c7e835338cef9b91310efbfcca1ce92
Fred Abenah <frederick.abenah@gmail.com>
2022-12-10 11:40:50 +0100

    Init

---
+++ src/main/java/com/barbedwire_back/back/BarbedWireBackApplication.java
@@ -0,0 +1,13 @@
+package com.barbedwire_back.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbedWireBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbedWireBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbedwire_back/back/BarbedWireBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbedwire_back.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbedWireBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
